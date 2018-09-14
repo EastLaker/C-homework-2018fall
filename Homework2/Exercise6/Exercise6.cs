f9a66e295d3d05e453ab0addf0fd96a517577053
unknown <ulysesschen@163.com>
2018-09-14 23:37:37 +0800

    create Homework2

---
+++ Homework2/Exercise6/Exercise6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+6.输出用户数据的所有素数因子
+7.求一个整数数组的最大值，最小值，平均值和所有数组元素的和
+9.用“埃氏筛法”求2~100以内的素数（先去掉2的倍数，再去掉3的倍数，再去掉4的倍数...最后剩下的就是素数）
+*/
+namespace Exercise6
+{
+    class Exercise6
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an intger: ");
+            int target=int.Parse(Console.ReadLine());
+            //int[] primes=new int[1000];
+            int i = 2;
+            while(target>=i)
+            {
+                if(target%i==0)
+                {
+                    target = target / i;
+                    Console.Write(i + " ");
+                }
+                if (target % i != 0)
+                {
+                    i++;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Homework2/Exercise7/Exercise7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    class Exercise7
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A Random Array:");
+            int[] array = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Random rd = new Random();
+                array[i] = rd.Next(0,1000);
+                Console.Write(array[i].ToString()+" ");
+                System.Threading.Thread.Sleep(15);
+            }
+            int max = array[0], min = array[0], length = array.Length,sum=0 ;
+            for(int i=0;i<length;i++)
+            {
+                if(array[i]>max)
+                { max = array[i]; }
+                if (array[i] <min )
+                { min = array[i]; }
+                sum += array[i];
+            }
+            double avg = sum / length;
+            Console.WriteLine("\n"+"Max: " + max + " Min: " + min + " Average: " + avg + " Sum:" + sum);
+            Console.ReadLine();                      
+        }
+    }
+}
