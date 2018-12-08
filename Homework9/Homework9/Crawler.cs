81355d5f2b665a1740cc82a7cfdd4d32449c4dce
unknown <ulysesschen@163.com>
2018-12-08 16:47:10 +0800

    fix some bugs

---
+++ Homework9/Homework9/Crawler.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Homework9
+{
+    public class Crawler
+    {
+        public Hashtable urls = new Hashtable();
+        private int count = 0;
+        Stopwatch stopwatch = new Stopwatch();
+        
+
+        public void Crawl()
+        {
+            Console.WriteLine("start crawling......");
+            stopwatch.Start();
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    current = url;
+                }
+                if (current == null || count > 100) break;
+
+                Console.WriteLine("Crawling " + current + " page!");
+                Task.Run(()=>Download(current));
+
+                //urls[current] = true;
+                count++;
+                
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Time: " + stopwatch.Elapsed);
+            Console.WriteLine("Crawl ends.");
+        }
+
+        public void Download(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                Parse(html);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //return "";
+            }
+        }
+
+        public void Parse(string html)
+        {
+            string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+            MatchCollection matchs = new Regex(strRef).Matches(html);
+            foreach (Match match in matchs)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf("=") + 1)
+                                                .Trim('"', '\"', '#', ',', '>');
+                if (strRef.Length == 0) continue;
+                if (urls[strRef] == null) urls[strRef] = false;
+            }
+        }
+    }
+}
---
+++ Homework9/Homework9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Homework9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Crawler myCrawer = new Crawler();
+            string startUrl = "http://www.cnblogs.com/dstang2000/";
+            if (args.Length >= 1) startUrl = args[0];
+
+            myCrawer.urls.Add(startUrl, false); //add start page
+            new Thread(myCrawer.Crawl).Start(); //start crawling
+            //new Thread(myCrawer.Crawl).Start();
+            
+            Console.Read();
+        }
+    }
+
+}
